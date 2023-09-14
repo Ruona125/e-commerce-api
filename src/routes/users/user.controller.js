81aa0cb5fb925f1a345dd8bb9ca79d058804bf21
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-09-14 12:42:37 +0100

    added the login functionality and implemeted jwt token

--- src/routes/users/user.controller.js
+++ src/routes/users/user.controller.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt")
 const { User } = require("../../models/userModels");
 
 async function registerUser(req, res) {
@@ -10,8 +12,11 @@ async function registerUser(req, res) {
       return res.status(400).json({ message: "Email already in use" });
     }
 
+    // Hash the user's password before saving it
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     // Create a new user
-    const user = new User({ username, email, password });
+    const user = new User({ username, email, password:hashedPassword });
 
     // Save the user to the database
     await user.save();
@@ -56,9 +61,43 @@ async function deleteCertainUser(req, res) {
     res.status(500).json({ message: error.message });
   }
 }
+
+async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+
+    // If the user does not exist, return an error
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    // Check if the provided password matches the stored password
+    if (user.password !== password) {
+      return res.status(401).json({ message: "Incorrect password" });
+    }
+
+    // Generate a JWT token containing the user's ID
+    const token = jwt.sign({ userId: user._id }, "your-secret-key", {
+      expiresIn: "1h", // Token expiration time (adjust as needed)
+    });
+    const decode = jwt.decode(token)
+    console.log(decode.userId)
+
+    // Return the token to the client
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
 module.exports = {
   registerUser,
   viewUsers,
   viewCertainUsers,
   deleteCertainUser,
+  login,
 };
--- src/routes/users/user.router.js
+++ src/routes/users/user.router.js
@@ -1,10 +1,11 @@
 const express = require("express")
-const {registerUser, viewUsers} = require("./user.controller")
+const {registerUser, viewUsers, login} = require("./user.controller")
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.get("/users", viewUsers);
+userRouter.post("/login", login);
 
 module.exports = {
     userRouter
