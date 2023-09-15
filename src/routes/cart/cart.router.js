9b43b9078c6fe57ce2df219cb02ed3e1e77b6532
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-09-15 12:07:49 +0100

    added the authorization middleware to the cart

--- src/routes/cart/cart.router.js
+++ src/routes/cart/cart.router.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const {createCart, viewCart,viewCertainCart, deleteCart} = require("./cart.controller")
-
+const {isAdmin, authorize} = require("../../utils/requireAuth")
 const cartRouter = express.Router();
 
-cartRouter.post("/cart", createCart);
-cartRouter.get("/cart", viewCart)
-cartRouter.get("/cart/:userId", viewCertainCart)
-cartRouter.delete("/cart/:id", deleteCart)
+cartRouter.post("/cart", authorize, createCart);
+cartRouter.get("/cart", authorize, viewCart)
+cartRouter.get("/cart/:userId", authorize, viewCertainCart)
+cartRouter.delete("/cart/:id", authorize, deleteCart)
 
 module.exports = {
     cartRouter
