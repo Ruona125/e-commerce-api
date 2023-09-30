94ee04976a946e15e1cc01ecedb9a57639eea5d2
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-09-30 22:41:30 +0100

    worked on the wishlist

--- src/routes/wishlists/wishlist.controller.js
+++ src/routes/wishlists/wishlist.controller.js
@@ -1,3 +1,4 @@
+const { Product } = require("../../models/productModels");
 const { Wishlist } = require("../../models/wishlistModels");
 
 async function createWishlist(req, res) {
@@ -18,15 +19,34 @@ async function viewWishlist(req, res) {
   }
 }
 
+//this is for the users view the wishlist that belongs to them
 async function viewCertainWishlist(req, res) {
   try {
     const { userId } = req.params;
-    const certainWishlist = await Wishlist.findOne({ userId });
-    if (certainWishlist) {
-      res.status(200).json(certainWishlist);
-    } else {
-      res.status(404).json({ message: "Wishlist not found" });
+
+    const wishlists = await Wishlist.find({ userId });
+
+    if (wishlists.length === 0) {
+      return res.status(404).json({ message: "wishlist not found" });
     }
+    const wishlistDetails = [];
+
+    for (const wishlist of wishlists) {
+      const productIds = wishlist.products.map((product) => product.productId);
+
+      const products = await Product.find({ _id: { $in: productIds } });
+
+      const wishlistWithProductDetails = {
+        userId: wishlist.userId,
+        products: wishlist.products.map((product) => ({
+          productId: product.productId,
+          quantity: product.quantity,
+          productDetails: products.find((p) => p._id.equals(product.productId)),
+        })),
+      };
+      wishlistDetails.push(wishlistWithProductDetails);
+    }
+    res.status(200).json(wishlistDetails)
   } catch (error) {
     res.status(400).json("wishlist not found");
   }
@@ -45,24 +65,26 @@ async function deleteWishlist(req, res) {
   }
 }
 
-async function modifyWishlist(req, res){
-    try{
-        const {id} = req.params;
-        const updatedWishlist = await Wishlist.findByIdAndUpdate(id, req.body, {new: true}) 
+async function modifyWishlist(req, res) {
+  try {
+    const { id } = req.params;
+    const updatedWishlist = await Wishlist.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
 
-        if(!updatedWishlist){
-            return res.status(404).json("can't find wishlist")
-        }
-        return res.status(200).json(updatedWishlist)
-    }catch(error){
-        res.status(400).json("error modifying wishlist")
+    if (!updatedWishlist) {
+      return res.status(404).json("can't find wishlist");
     }
+    return res.status(200).json(updatedWishlist);
+  } catch (error) {
+    res.status(400).json("error modifying wishlist");
+  }
 }
 
-module.exports ={
-    createWishlist, 
-    viewCertainWishlist,
-    viewWishlist,
-    modifyWishlist,
-    deleteWishlist
-}
+module.exports = {
+  createWishlist,
+  viewCertainWishlist,
+  viewWishlist,
+  modifyWishlist,
+  deleteWishlist,
+};
