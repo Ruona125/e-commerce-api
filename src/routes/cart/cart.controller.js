650af1f33203eeeb0027b9cb9ef0fe1510286217
ruona <ogheneruonaagadagba4@gmail.com>
2023-10-25 15:41:43 +0100

    made a minor change

--- src/routes/cart/cart.controller.js
+++ src/routes/cart/cart.controller.js
@@ -23,58 +23,6 @@ async function viewCart(req, res) {
 }
 
 //this is the one that returns an array
-// async function viewCertainUserCart(req, res) {
-//   try {
-//     const { userId } = req.params;
-
-//     // Find all carts that belong to the user based on userId
-//     const carts = await Cart.find({ userId });
-
-//     if (carts.length === 0) {
-//       return res.status(404).json({ message: "Carts not found" });
-//     }
-
-//     // Initialize an array to store cart details
-//     const cartDetails = [];
-
-//     for (const cart of carts) {
-//       const productIds = cart.products.map((product) => product.productId);
-
-//       const products = await Product.find({ _id: { $in: productIds } });
-
-//       // Combine the cart and product details
-//       const cartWithProductDetails = {
-//         id: cart.id,
-//         userId: cart.userId,
-//         products: cart.products.map((product) => ({
-//           productId: product.productId,
-//           quantity: product.quantity,
-//           productDetails: products.find((p) => p._id.equals(product.productId)),
-//         })),
-//       };
-
-//       cartDetails.push(cartWithProductDetails);
-//     }
-
-//     // Calculate the total cart count
-//     const totalCartCount = cartDetails.length;
-
-//     // Return the response as an array including totalCartCount
-//     const response = [
-//       {
-//         totalCarts: totalCartCount,
-//         cartDetails: cartDetails,
-//       },
-//     ];
-
-//     res.status(200).json(response);
-//   } catch (error) {
-//     console.error(error); // Log the error for debugging
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// }
-
-//this is the one that returns an object
 async function viewCertainUserCart(req, res) {
   try {
     const { userId } = req.params;
@@ -111,11 +59,13 @@ async function viewCertainUserCart(req, res) {
     // Calculate the total cart count
     const totalCartCount = cartDetails.length;
 
-    // Return the cart details as an array along with totalCartCount
-    const response = {
-      totalCarts: totalCartCount,
-      cartDetails: cartDetails,
-    };
+    // Return the response as an array including totalCartCount
+    const response = [
+      {
+        totalCarts: totalCartCount,
+        cartDetails: cartDetails,
+      },
+    ];
 
     res.status(200).json(response);
   } catch (error) {
@@ -124,13 +74,55 @@ async function viewCertainUserCart(req, res) {
   }
 }
 
+//this is the one that returns an object
+// async function viewCertainUserCart(req, res) {
+//   try {
+//     const { userId } = req.params;
 
+//     // Find all carts that belong to the user based on userId
+//     const carts = await Cart.find({ userId });
 
+//     if (carts.length === 0) {
+//       return res.status(404).json({ message: "Carts not found" });
+//     }
 
+//     // Initialize an array to store cart details
+//     const cartDetails = [];
 
+//     for (const cart of carts) {
+//       const productIds = cart.products.map((product) => product.productId);
 
+//       const products = await Product.find({ _id: { $in: productIds } });
 
+//       // Combine the cart and product details
+//       const cartWithProductDetails = {
+//         id: cart.id,
+//         userId: cart.userId,
+//         products: cart.products.map((product) => ({
+//           productId: product.productId,
+//           quantity: product.quantity,
+//           productDetails: products.find((p) => p._id.equals(product.productId)),
+//         })),
+//       };
 
+//       cartDetails.push(cartWithProductDetails);
+//     }
+
+//     // Calculate the total cart count
+//     const totalCartCount = cartDetails.length;
+
+//     // Return the cart details as an array along with totalCartCount
+//     const response = {
+//       totalCarts: totalCartCount,
+//       cartDetails: cartDetails,
+//     };
+
+//     res.status(200).json(response);
+//   } catch (error) {
+//     console.error(error); // Log the error for debugging
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// }
 
 async function deleteCart(req, res) {
   try {
@@ -150,7 +142,7 @@ async function modifyCart(req, res) {
   try {
     const { id } = req.params;
     const { userId } = req.user;
-    
+
     // Check if the user has the correct permissions to modify the cart
     const cart = await Cart.findById(id);
     if (!cart) {
@@ -158,11 +150,15 @@ async function modifyCart(req, res) {
     }
 
     if (cart.userId !== userId) {
-      return res.status(403).json("Unauthorized: You can only modify your own cart");
+      return res
+        .status(403)
+        .json("Unauthorized: You can only modify your own cart");
     }
 
     // Update the cart based on the request body
-    const updatedCart = await Cart.findByIdAndUpdate(id, req.body, { new: true });
+    const updatedCart = await Cart.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
 
     return res.status(200).json(updatedCart);
   } catch (error) {
@@ -176,29 +172,26 @@ const userCartCounts = {};
 // Middleware to extract user ID from JWT token
 function getUserIdFromToken(token) {
   const decodedToken = jwt.verify(token, "your-secret-key");
-  console.log(decodedToken.userId)
-  console.log("hello")
+  console.log(decodedToken.userId);
+  console.log("hello");
   return decodedToken.userId;
 }
 
-async function cartCount(req, res){
-  try{
-    
+async function cartCount(req, res) {
+  try {
     if (!req.headers.authorization) {
       return res.status(401).json({ message: "Unauthorized" });
     }
-  
+
     const userId = getUserIdFromToken(req.headers.authorization);
     const cartCount = userCartCounts[userId] || 0;
-  
+
     res.status(200).json({ count: cartCount });
-  }catch(error){
-    console.log(error.message)
-    res.status(500).json({message: error.message})
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
   }
 }
-
-
 
 module.exports = {
   createCart,
@@ -206,5 +199,5 @@ module.exports = {
   viewCertainUserCart,
   deleteCart,
   modifyCart,
-  cartCount
+  cartCount,
 };
