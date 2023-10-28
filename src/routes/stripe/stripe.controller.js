f2431b231ea959e9f2cc898d8378bb61b9cf2142
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-10-28 21:04:17 +0100

    worked on implementing the payment

--- src/routes/stripe/stripe.controller.js
+++ src/routes/stripe/stripe.controller.js
@@ -1,23 +1,33 @@
+require('dotenv').config();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
-function stripePayment(req, res) {
+async function stripePayment(req, res) {
+  //   try {
+  //     stripe.charges.create(
+  //       {
+  //         source: req.body.tokenId,
+  //         amount: req.body.amount,
+  //         currency: "usd",
+  //       },
+  //       (stripeErr, stripeRes) => {
+  //         if (stripeErr) {
+  //           res.status(500).json(stripeErr);
+  //         } else {
+  //           res.status(200).json(stripeRes);
+  //         }
+  //       }
+  //     );
+  //   } catch (error) {
+  //     res.status(400).json("error making payment");
+  //   }
   try {
-    stripe.charges.create(
-      {
-        source: req.body.tokenId,
-        amount: req.body.amount,
-        currency: "usd",
-      },
-      (stripeErr, stripeRes) => {
-        if (stripeErr) {
-          res.status(500).json(stripeErr);
-        } else {
-          res.status(200).json(stripeRes);
-        }
-      }
-    );
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: req.body.amount,
+      currency: "USD",
+    });
+    res.json({ clientSecret: paymentIntent.client_secret });
   } catch (error) {
-    res.status(400).json("error making payment");
+    res.status(500).json({ error: error.message });
   }
 }
 
