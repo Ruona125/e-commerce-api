a544d4686f32eee6754fda5d439af7c283e49920
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-10-30 11:02:34 +0100

    fixed the stripe issue

--- src/routes/stripe/stripe.controller.js
+++ src/routes/stripe/stripe.controller.js
@@ -1,32 +1,53 @@
-require('dotenv').config();
+require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 async function stripePayment(req, res) {
-  //   try {
-  //     stripe.charges.create(
-  //       {
-  //         source: req.body.tokenId,
-  //         amount: req.body.amount,
-  //         currency: "usd",
-  //       },
-  //       (stripeErr, stripeRes) => {
-  //         if (stripeErr) {
-  //           res.status(500).json(stripeErr);
-  //         } else {
-  //           res.status(200).json(stripeRes);
-  //         }
+  // try {
+  //   stripe.charges.create(
+  //     {
+  //       source: req.body.tokenId,
+  //       amount: req.body.amount,
+  //       currency: "usd",
+  //     },
+  //     (stripeErr, stripeRes) => {
+  //       if (stripeErr) {
+  //         res.status(500).json(stripeErr);
+  //       } else {
+  //         res.status(200).json(stripeRes);
   //       }
-  //     );
-  //   } catch (error) {
-  //     res.status(400).json("error making payment");
-  //   }
+  //     }
+  //   );
+  // } catch (error) {
+  //   res.status(400).json("error making payment");
+  // }
+
+  //this is the one that worked
+  // try {
+  //   const paymentIntent = await stripe.paymentIntents.create({
+  //     amount: req.body.amount,
+  //     currency: "USD",
+  //   });
+  //   res.json({ clientSecret: paymentIntent.client_secret });
+  // } catch (error) {
+  //   res.status(500).json({ error: error.message });
+  // }
+
   try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: req.body.amount,
-      currency: "USD",
+    const { token, amount, description } = req.body;
+
+    // Create a charge using the Stripe API
+    const charge = await stripe.charges.create({
+      amount, // Amount in cents
+      currency: "CAD",
+      source: token.id, // Token from the client
+      description,
     });
-    res.json({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
+
+    // You can save the charge object in your database for reference
+    // Handle success or error here
+
+    res.status(200).json({ message: "Payment successful" });
+  } catch (err) {
     res.status(500).json({ error: error.message });
   }
 }
