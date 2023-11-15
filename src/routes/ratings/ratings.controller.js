77320f9ef1fef09735a9c56cf28c149d14297a12
ruona <ogheneruonaagadagba4@gmail.com>
2023-11-15 15:04:01 +0100

    worked on the rating controller

--- src/routes/ratings/ratings.controller.js
+++ src/routes/ratings/ratings.controller.js
@@ -10,15 +10,36 @@ async function createRating(req, res){
     }
 }
 
-async function viewRatings(req, res){
-    try{
-        const ratings = await Rating.find({})
-        res.status(200).json(ratings);
-    }catch(error){
-        console.log(error);
-        res.status(500).json("error")
+//this is the normal one
+// async function viewRatings(req, res){
+//     try{
+//         const ratings = await Rating.find({})
+//         res.status(200).json(ratings);
+//     }catch(error){
+//         console.log(error);
+//         res.status(500).json("error")
+//     }
+// }
+
+//this is the one that'll return the average
+async function viewRatings(req, res) {
+    try {
+      const ratings = await Rating.aggregate([
+        {
+          $group: {
+            _id: "$productId",
+            averageRating: { $avg: "$ratings" },
+            allRatings: { $push: "$$ROOT" }, // This will include all rating documents in the result
+          },
+        },
+      ]);
+  
+      res.status(200).json(ratings);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json("error");
     }
-}
+  }
 
 module.exports = {
     createRating, 
