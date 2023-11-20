652de15339d5641fb9528d21f6dfbf56132f52f1
ruona <ogheneruonaagadagba4@gmail.com>
2023-11-20 13:53:15 +0100

    added the new database

--- server.js
+++ server.js
@@ -18,4 +18,9 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
- 
+
+  // development database
+  // mongodb+srv://bucollections:grEivUMladnVWeI4@bucollections.fsatjk8.mongodb.net/bucollections?retryWrites=true&w=majority
+
+  //production database
+ // mongodb://mongo:f-1gah4A6A-3a4H3C14fBB4Dh63CbEc4@roundhouse.proxy.rlwy.net:59804
